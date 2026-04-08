eb656d3d52040a17594653541c1fd53153603dfe
Bobw147 <bobw147@gmail.com>
2026-04-08 16:37:03 +0100

    removed unwanted and defunct test code

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Editor, MarkdownView, Plugin, ViewStateResult } from 'obsidian';
+import { MarkdownView, Plugin } from 'obsidian';
 
 import { CodeBlockProcessor } from './code-block-processor';
 import { DEFAULT_SETTINGS, InPageTabsSettings, InPageTabsSettingTab } from './settings';
@@ -21,40 +21,17 @@ export default class InPageTabs extends Plugin {
             this.codeBlockProcessor = new CodeBlockProcessor();
             this.runCount++;    // Increment runCount to ensure unique radio group names for each code block instance.
             this.codeBlockProcessor.process(this.app, source, el, ctx, this.runCount);
+
+            // Now the codeblock processor exists it is safe to start the interval timer.
+            // Ensure, however that only a single interval timer is started
             if (!this.intervalTimerRunning){
 		        this.registerInterval(window.setInterval(() => { void this.intervalTimer(); },  500));
                 this.intervalTimerRunning = true;
             }
         });
 
-        // This adds an editor command that can perform some operation on the current editor instance
-        this.addCommand({
-			id: 'replace-selected',
-			name: 'Replace selected content',
-			editorCallback: (editor: Editor, ctx: MarkdownView) => {
-				editor.replaceSelection('Sample editor command');
-			}
-		});
-
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new InPageTabsSettingTab(this.app, this));
-
-        this.registerDomEvent(document, 'change', (event) => {
-            const target = event.target as HTMLElement;
-            if (target.tagName === 'INPUT' && (target as HTMLInputElement).type === 'radio') {
-                const instances = document.getElementsByName((target as HTMLInputElement).name);
-                instances.forEach(instance => {
-                    if (instance.id == target.id) {
-                        if ((target as HTMLInputElement).checked && !(instance as HTMLInputElement).checked) {
-                            (instance as HTMLInputElement).checked = true;
-                        }
-                        else if (!(target as HTMLInputElement).checked && (instance as HTMLInputElement).checked) {
-                            (target as HTMLInputElement).checked = true;
-                        }
-                    }
-                });
-            }
-        });
 	}
 
 	async loadSettings() {
@@ -66,8 +43,6 @@ export default class InPageTabs extends Plugin {
 	}
     
     async intervalTimer() {
-        // TODO This implementation is currently agnostic in respect of the actual
-        // tab group.
         // This interval timer is currently used to check whether the view mode has changed,
         // and if so, update the tab visibility accordingly.
         // This is a workaround for the fact that there is no event that is triggered when the
@@ -75,7 +50,8 @@ export default class InPageTabs extends Plugin {
         // as this indicates Live Preview mode so the alternate buttons are active but do not
         // correspond with the diplayed set.
         const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-        if (markdownView)
-            await this.codeBlockProcessor?.tabManager?.monitorTabActivity(markdownView);
+        if (markdownView && this.codeBlockProcessor && this.codeBlockProcessor.tabManager) {
+            await this.codeBlockProcessor.tabManager.monitorTabActivity(markdownView);
+        }
     }
 }
