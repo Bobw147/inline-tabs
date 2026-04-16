0a891f8ff10c035d0b5ebc6c1922504ee21c8591
Bobw147 <bobw147@gmail.com>
2026-04-16 23:41:16 +0100

    Significant refactoring of code to  make the next stepeasier to implement

--- src/main.ts
+++ src/main.ts
@@ -18,93 +18,20 @@ export default class InPageTabs extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-/*
-        this.registerMarkdownCodeBlockProcessor('page-tabs', (source, el, ctx) => {
-            this.codeBlockProcessor = new CodeBlockProcessor();
-            this.runCount++;    // Increment runCount to ensure unique radio group names for each code block instance.
-            this.codeBlockProcessor.process(this.app, source, el, ctx, this.runCount);
-
-            // Now the codeblock processor exists it is safe to start the interval timer.
-            // Ensure, however that only a single interval timer is started
-            if (!this.intervalTimerRunning){
-		        this.registerInterval(window.setInterval(() => { void this.intervalTimer(); },  500));
-                this.intervalTimerRunning = true;
-            }
-        });
-*/
-
-        // This registers a markdown post processor which looks for code blocks with the language 'page-tabs' and processes them using the CodeBlockProcessor.
+        // Register a markdown post processor
         this.registerMarkdownPostProcessor((container, ctx) => {
-            const tabsContainer: HTMLElement[] = this.findTabStarts(container);
-            if (tabsContainer.length === 0) return;
+            // See if this leaf contains any tab declarations
+            const tabContainers: HTMLElement[] = this.findTabStarts(container);
+            if (tabContainers.length === 0) return;
+
+            // If so, process the tab declarations and insert them into the DOM.
             const tabsPostProcessor = new TabsPostProcessor(this.app, container, ctx);
-            tabsPostProcessor.process2(tabsContainer);
+            tabsPostProcessor.process2(container, tabContainers);
         });
-/*
-            // Check to see if there any tabstart markers in the container.
-            const tabStarts: Element[] = this.findElementsWithText(container, '<>tabs-start');
-            if (tabStarts.length === 0) return;
 
-            const tabsPostProcessor = new TabsPostProcessor(this.app, container, ctx, )
-            tabsPostProcessor.process(tabStarts);
-        });
-*/
- /*           const tabStarts = this.findElementsWithText(container, '<>tabs-start');
-            if (tabStarts.length === 0) return;
-            for (const tabStart of tabStarts) {
-                const contentEnd = tabStart.textContent?.indexOf('<>tabs-end');
-                if (contentEnd === undefined || contentEnd === -1) break;
-            
-                const source = tabStart.textContent?.substring('<>tabs-start'.length, contentEnd);
-                if (!source) continue;
-
-                const header: string[] = source.split('\n');
-                let tabGroupName: string = '';
-                let labelText: string = '';
-                let checked: boolean = false;
-                let content: string = '';
-                try {
-                    for (const line of header) {
-                        const [key, value] = line.split(':').map(part => part.trim());
-                        if (!key || !value) continue;
-
-                        if (key.toLowerCase() == 'tabgroupname') tabGroupName = value;
-                        else if (key.toLowerCase() == 'label') labelText = value;
-                        else if (key.toLowerCase() == 'checked') checked = (value.toLowerCase() == 'true');
-                        else if (key.toLowerCase() == 'content') content = value;
-                    }
-                    if (!tabGroupName || !labelText) continue;
-
-                    const tabsDiv = container.createEl('div', {cls: 'inline-tabs'});
-                    const tabInput = tabsDiv?.createEl('input', {type: 'radio'});
-                    if (!tabInput ) continue;
-                    tabInput.name = tabGroupName;
-                    tabInput.id = `${tabGroupName}-${labelText}`;
-                    tabInput.checked = checked;
-                    tabsDiv.createEl('label', {text: labelText, attr: {for: tabInput.id}});
-
-                    const tabInner = tabsDiv.createEl('div', {cls: 'inline-tab'})
-                    tabInner.createEl('span', {text: content});
-
-                    // Find the embedded item in the DOMM and relocate inside the tab content space
-                    const embeddedItem = container.querySelector('.internal-embed');
-                    console.log(tabStarts); // Outputs matching elements
-                } catch (error) {
-                    // Any error in the header that makes it invalid remains as plain text to show the error to the user
-                    // and processing continues for any other valid tab blocks in the same file.
-                    continue;
-                }
-            }
-        });
-*/        
         // This adds a settings tab so the user can configure various aspects of the plugin
         this.addSettingTab(new InPageTabsSettingTab(this.app, this));
 	}
-
-    findElementsWithText(container: HTMLElement, searchText: string) {
-        return Array.from(container.querySelectorAll('text'))
-        .filter(element => element.textContent.includes(searchText));
-    }
 
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData() as Partial<InPageTabsSettings>);
@@ -136,56 +63,6 @@ export default class InPageTabs extends Plugin {
             }
         });
         return tabStarts;
-    }
-
-    tabProcessor(container: HTMLElement) {
-        const tabStarts = this.findTabStarts(container);
-
-        if (tabStarts.length === 0) return;
-        for (const tabStart of tabStarts) {
-            const contentEnd = tabStart.textContent?.indexOf('<>tabs-end');
-            if (contentEnd === undefined || contentEnd === -1) break;
-            
-            const source = tabStart.textContent?.substring('<>tabs-start'.length, contentEnd);
-            if (!source) continue;
-
-            const header: string[] = source.split('\n');
-            let tabGroupName: string = '';
-            let labelText: string = '';
-            let checked: boolean = false;
-            let content: string = '';
-            try {
-                for (const line of header) {
-                    const [key, value] = line.split(':').map(part => part.trim());
-                    if (!key || !value) continue;
-
-                    if (key.toLowerCase() == 'tabgroupname') tabGroupName = value;
-                        else if (key.toLowerCase() == 'label') labelText = value;
-                        else if (key.toLowerCase() == 'checked') checked = (value.toLowerCase() == 'true');
-                        else if (key.toLowerCase() == 'content') content = value;
-                }
-                if (!tabGroupName || !labelText) continue;
-
-                const tabsDiv = container.createEl('div', {cls: 'inline-tabs'});
-                const tabInput = tabsDiv?.createEl('input', {type: 'radio'});
-                if (!tabInput ) continue;
-                tabInput.name = tabGroupName;
-                tabInput.id = `${tabGroupName}-${labelText}`;
-                tabInput.checked = checked;
-                tabsDiv.createEl('label', {text: labelText, attr: {for: tabInput.id}});
-
-                const tabInner = tabsDiv.createEl('div', {cls: 'inline-tab'})
-                tabInner.createEl('span', {text: content});
-
-                // Find the embedded item in the DOMM and relocate inside the tab content space
-                const embeddedItem = container.querySelector('.internal-embed');
-                console.log(tabStarts); // Outputs matching elements
-            } catch (error) {
-                // Any error in the header that makes it invalid remains as plain text to show the error to the user
-                // and processing continues for any other valid tab blocks in the same file.
-                continue;
-            }
-        }
     }
 }
 
--- src/tabsPostProcessor.ts
+++ src/tabsPostProcessor.ts
@@ -14,9 +14,13 @@ export class TabsPostProcessor {
         this._tabManager = new TabManager(app, container);
     }
 
-    isMarker(node: ChildNode, propertyType: string): boolean {
-        if (node.nodeName === '#text' && node.textContent != null) {
-            return node.textContent.trim().toLowerCase() === propertyType.toLowerCase();
+    isMarker(node: ChildNode, propertyType: string, stripleadingNewlines: boolean = false): boolean {
+        if (node.nodeName === '#text'&& node.textContent != null) {
+            let textContent = node.textContent;
+            if (stripleadingNewlines) {
+                textContent = textContent.replace(/^[\r\n]+/, '');
+            }
+            return textContent.trim().toLowerCase() === propertyType.toLowerCase();
         }
         return false;
     }
@@ -29,8 +33,8 @@ export class TabsPostProcessor {
         return this.isMarker(node, 'content-end');
     }
 
-    isTabsStart(node: ChildNode): boolean {
-        return this.isMarker(node, '<>tabs-start');
+    isTabsStart(node: ChildNode, removeBR: boolean = false): boolean {
+        return this.isMarker(node, '<>tabs-start', removeBR);
     }
 
     isTabsEnd(node: ChildNode): boolean {
@@ -69,7 +73,38 @@ export class TabsPostProcessor {
         return this.isProperty(node, 'label');
     }
 
-    process2(tabsContainer: Element[]) {
+    nextSibling(currentNode: ChildNode | null, removeBR: boolean): ChildNode | null {
+        let node: ChildNode | null = (currentNode ? currentNode.nextSibling : null);
+        while (node) {
+            if (node.nodeName === 'BR' && removeBR) {
+                const next: ChildNode | null = node.nextSibling;
+                node.remove();
+                node = next;
+            }
+            else
+                break;
+        }
+        return node;
+    }
+
+    process2(DOMContainer: HTMLElement, tabContainers: Element[]) {
+        // DOMContainer is a 'div el-p' that contains all the declarations of all the top level tab groupings
+        // tabContainers is an array of HTMLElelemnts that each contain the declarations for a single tab group.
+        // There may be multiple tabContainers within a single DOMContainer.
+        // if there are multiple top level tab groupings within a single leaf each tabContainer needs to be processed
+        // separately to ensure that the tabs are grouped correctly.
+        // Find all the tab groups in this leaf and process them
+        for (const tabContainer of tabContainers) {
+            if ((!tabContainer) || tabContainer.childNodes.length === 0) continue;
+            let childNode: ChildNode | null = tabContainer.childNodes[0] as ChildNode;
+            while (childNode && !this.isTabsStart(childNode, true)) {
+                childNode = this.nextSibling(childNode, true) as ChildNode;
+            }
+            this.processTabGroup(DOMContainer, childNode);
+        }
+    }
+
+    processTabGroup(DOMContainer: HTMLElement, childNode: ChildNode | null) {
         let inTabsDef: boolean = false;
         let inTabDef: boolean = false;
         let tabGroupName: string = ``;
@@ -77,162 +112,98 @@ export class TabsPostProcessor {
         let labelText: string = ``;
         let checked: boolean = false;
         let content: HTMLElement[] = [];
-
-        for (const container of tabsContainer) {
-            let childNode: ChildNode | undefined | null = container.childNodes[0];
-            do {
-                if (!childNode) break;
-                if (childNode.nodeName === 'BR') {
-                    childNode = childNode.nextSibling;
-                    continue;
-                }
-                switch (inTabsDef) {
-                    case false:
-                        if (this.isTabsStart(childNode)) {
-                            inTabsDef = true;
-                            inTabDef = false;
-                            tabGroupName = `${uuidv4()}`;
-                        }
+    
+        // On entry, childNode should be the tabsStarts node.
+        while (childNode) {
+            switch (inTabsDef) {
+                case false:
+                    if (this.isTabsStart(childNode)) {
+                        inTabsDef = true;
+                        inTabDef = false;
+                        tabGroupName = `${uuidv4()}`;
+                    }
+                    break;
+                case true:
+                    // <>tabs-end as highest priority to end tabs definition
+                    if (this.isTabsEnd(childNode)){
+                        inTabsDef = false;
+                        tabGroupName = ``;
                         break;
-                    case true:
-                        // <>tabs-end as highest priority to end tabs definition
-                        if (this.isTabsEnd(childNode)){
-                            inTabsDef = false;
-                            tabGroupName = ``;
-                            break;
-                        } else {
-                            switch (inTabDef) {
-                                case false:
-                                    if (this.isTabStart(childNode)) {
-                                        inTabDef = true;
-                                        tabId = `${uuidv4()}`;
-                                    }
-                                    break;
-                                case true: {
-                                    const [isLabel, value] = this.isLabel(childNode);
-                                    if (isLabel) {
-                                        labelText = value;
-                                        break;
-                                    }
-                                    const [isChecked, checkedValue] = this.isChecked(childNode);
-                                    if (isChecked) {
-                                        checked = checkedValue.toLowerCase() === 'true';
-                                        break;
-                                    }
-                                    if (this.isContentStart(childNode)) {
-                                        // Create a document fragment to hold the content as we may need
-                                        // to move multiple nodes into the content of the tab.
-                                        const [isolatedContent, newChildNode] = this.processContent(childNode);
-                                        content.push(...isolatedContent);
-                                        childNode = newChildNode;
-                                        break
-                                    }
-                                    if (this.isTabEnd(childNode)) {
-                                        inTabDef = false;
-
-                                        // See if we have the minimum neccessary to create a tab
-                                        if (labelText == '')
-                                            labelText = 'Tab';
-                                        if (content.length === 0) {
-                                            const defaultContent: HTMLElement = document.createElement('div');
-                                            defaultContent.textContent = `Content for ${labelText}`;
-                                            content.push(defaultContent);
-                                        }
-                        
-                                        this._tabManager?.createTab(tabGroupName, tabId, labelText, checked, content);
-                                        
-                                        // Reset to default values for next tab definition
-                                        tabId = ``;
-                                        labelText = ``;
-                                        checked = false;
-                                        content = [];
-                                    }
+                    } else {
+                        switch (inTabDef) {
+                            case false:
+                                if (this.isTabStart(childNode)) {
+                                    inTabDef = true;
+                                    tabId = `${uuidv4()}`;
+                                }
+                                break;
+                            case true: {
+                                const [isLabel, value] = this.isLabel(childNode);
+                                if (isLabel) {
+                                    labelText = value;
                                     break;
                                 }
+                                const [isChecked, checkedValue] = this.isChecked(childNode);
+                                if (isChecked) {
+                                    checked = checkedValue.toLowerCase() === 'true';
+                                    break;
+                                }
+                                if (this.isContentStart(childNode)) {
+                                    // Create a document fragment to hold the content as we may need
+                                    // to move multiple nodes into the content of the tab.
+                                    const [isolatedContent, nextChildNode] = this.processContent(childNode);
+                                    content.push(...isolatedContent);
+                                    childNode = nextChildNode as ChildNode;
+                                    break;
+                                }
+                                if (this.isTabEnd(childNode)) {
+                                    inTabDef = false;
+
+                                    // See if we have the minimum neccessary to create a tab
+                                    if (labelText == '')
+                                        labelText = 'Tab';
+                                    if (content.length === 0) {
+                                        const defaultContent: HTMLElement = document.createElement('div');
+                                        defaultContent.textContent = `Content for ${labelText}`;
+                                        content.push(defaultContent);
+                                    }
+                        
+                                    this._tabManager?.createTab(tabGroupName, tabId, labelText, checked, content);
+                                        
+                                    // Reset to default values for next tab definition
+                                    tabId = ``;
+                                    labelText = ``;
+                                    checked = false;
+                                    content = [];
+                                }
+                                break;
                             }
                         }
-                        break;
+                    }
+                    break;
                 }
-                childNode = childNode.nextSibling;
-            } while (childNode);
-        }
+                if (childNode) {
+                    let nextNode: ChildNode = this.nextSibling(childNode, true) as ChildNode;
+                    childNode.remove();
+                    childNode = nextNode;
+                }
+            }
     }
-
+    
     processContent(contentStartNode: ChildNode): [HTMLElement[], ChildNode | null] {
         const content: HTMLElement[] = [];
-        if (contentStartNode.nextSibling == null) return [content, null];
 
-        let node: ChildNode = contentStartNode.nextSibling;
+        let node: ChildNode | null = this.nextSibling(contentStartNode, true);
+        contentStartNode.remove();
         while (node && !this.isContentEnd(node)) {
-            content.push(node.cloneNode(true) as HTMLElement);
-
-            if (node.nextSibling)
-                node = node.nextSibling;
-            else
-                break;  
+            const clone = node.cloneNode(true) as HTMLElement;
+            content.push(clone);
+            let nextNode: ChildNode | null = this.nextSibling(node, true);
+            node.remove();
+            node = nextNode;
         }
-        return [content, node];
-    }
-
-    process(tabStarts: Element[]) {
-        for (const tabBlock of tabStarts) {
-            let tabGroupName: string = `${uuidv4()}`;
-            const contentEnd = tabBlock.textContent?.indexOf('<>tabs-end');
-            if (contentEnd === undefined || contentEnd === -1) continue;
-            const source = tabBlock.textContent?.substring('<>tabs-start'.length+1, contentEnd-('<>tabs-end'.length + 1));
-            this.processTabBlocks(tabGroupName, source);
-            console.log(source);
-        }
-    }
-
-    processTabBlocks(tabGroupName: string, source: string) {
-        const tabBlock: string[] = source.split('<>tab-start');
-        for (const tabInfo of tabBlock) {
-            if (tabInfo.trim().length === 0) continue;
-            this.processTabInfo(tabGroupName, tabInfo);
-        }
-        console.log(tabBlock);
-    }
-
-    processTabInfo(tabGroupName: string, tabInfo: string) {
-        let labelText: string = '';
-        let checked: boolean = false;
-        let content: string = '';
-        let tabId: string = "";
-
-        const lines: string[] = tabInfo.split('\n');
-        if (lines.length === 0) return;
-        lines.forEach((line, index) => {
-            if (line.trim().length === 0) return;
-            const [key, value] = line.split(':').map(part => part.trim());
-
-            if (!key || !value) return;
-            switch (key.toLowerCase()) {
-                case 'tabgroupname':
-                    tabGroupName = value;
-                    break;
-                case 'labeltext':
-                    labelText = value;
-                    break;
-                case 'checked':
-                    checked = value.toLowerCase() === 'true';
-                    break;
-                case 'content':
-                    content = value;
-                    break;
-                case '<>tab-end':
-                    // Handle tab end if needed
-                    break;
-                default:
-                    break;
-            }
-        });
-        if (labelText == '')
-            labelText = 'Tab';
-        if (content == '')
-            content = 'Content for ' + labelText;
-        tabId = `${uuidv4()}`;
-        console.log(lines);
-        this._tabManager?.createTab(tabGroupName, tabId, labelText, checked, content);
+        return [content, node];  
     }
 }
+
+
